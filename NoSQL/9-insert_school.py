0920ac9e001a4a16aa0f6f86e94a7c09887ec7dd
1Sultanz <z.r.amin40@gmail.com>
2025-07-07 21:03:12 +0400

    task

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,5 +3,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """Insert a document to a MongoDB collection"""
-    doc = list(mongo_collection.insert(**kwargs)
-               return doc
+    doc = list(mongo_collection.insert(kwargs)
+               return doc.inserted_id
