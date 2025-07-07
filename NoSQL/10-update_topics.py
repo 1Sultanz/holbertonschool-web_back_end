648593a4ab8bce69d529fd53c80b0cf2fd6e6682
1Sultanz <z.r.amin40@gmail.com>
2025-07-07 21:19:06 +0400

    task

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Change school topics"""
 
- def update_topics(mongo_collection, name, topics):
-     """Update topics of school"""
-     doc = mongo_collection.update_one(name, topics)
-     return doc
+def update_topics(mongo_collection, name, topics):
+    """Update topics of school"""
+    doc = mongo_collection.update_many(name, topics)
+    return doc
