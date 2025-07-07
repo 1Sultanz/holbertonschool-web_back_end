d2da7ca0f2de63620e7af87de3498482fb45af2e
1Sultanz <z.r.amin40@gmail.com>
2025-07-07 20:36:33 +0400

    task

---
+++ NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection"""
+    documents = list(mongo_collection.find())
+    return documents
